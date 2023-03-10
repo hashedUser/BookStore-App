7dac7b4d43bef27fef1dd3617fc51be482bb5f2a
adityakul411 <adityakul411@gmail.com>
2023-03-10 12:16:59 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import utils.database as database
+
+USER_CHOICE = """
+Enter:
+-- 'a' to add a new book
+-- 'l' to list all books
+-- 'r' to mark a book as read
+-- 'd' to delete a book 
+-- 'q' to quit
+
+Your Choice:"""
+
+
+def menu():
+    user_input = input(USER_CHOICE)
+
+
+
+
+menu()
---
+++ utils/database.py
@@ -0,0 +1,23 @@
+books = []
+
+
+def add_book(book, author):
+    books.append({'name': book, 'author': author, 'read': False})
+    return books
+
+
+def retrieve_books():
+    print(books)
+
+
+# mark something as read will change the read from false to true
+def mark_as_read(book_name):
+    for index in range(0, len(books)):
+        if book_name == books[index]['name']:
+            books[index]['read'] = True
+
+
+def delete_book(book_name):
+    for index in range(0, len(books)):
+        if book_name == books[index]['name']:
+            books.remove(books[index])
