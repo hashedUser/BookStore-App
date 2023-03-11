0eb0ae720ea4518ef1179278fc8e8cb27c918ec0
adityakul411 <adityakul411@gmail.com>
2023-03-11 15:05:18 +0530

    File integration changes.

--- utils/database.py
+++ utils/database.py
@@ -1,29 +1,43 @@
-books = []
-
-""" 
-Next Commit Changes:
-Add file integration so that the database won't be a 
-python list, but it would be a file on the storage
-so that for every execution the file will be changed
-and wouldn't be reset to its original form
 """
+Concerned with storing and retrieving books from a csv file
+Format of the csv file:
+
+name,author,read
+"""
+
+books_file = "books.txt"
 
 
 def add_book(book, author):
-    books.append({'name': book, 'author': author, 'read': False})
+    with open(books_file, 'a') as file:
+        file.write(f'{book},{author},0\n')
+        ...
 
 
 def retrieve_books():
-    return books
+    with open(books_file, "r") as file:
+        lines = [line.strip().split(",") for line in file.readlines()]
+
+    return [
+        {'name': line[0], 'author': line[1], 'read': line[2]} for line in lines
+    ]
 
 
 # mark something as read will change the read from false to true
 def mark_as_read(book_name, author_name):
+    books = retrieve_books()
     for book in books:
         if book['name'] == book_name & book['author'] == author_name:
-            book['read'] = True
+            book['read'] = "1"
+    _save_all_books(books)
+
+
+def _save_all_books(books):
+    with open(books_file, 'w') as file:
+        for book in books:
+            file.write(f"{book['name']},{book['author']},{book['read']}\n")
+
 
 
 def delete_book(book_name, author_name):
-    global books
-    books = [book for book in books if book['name'] != book_name & book['author'] != author_name]
+    ...
